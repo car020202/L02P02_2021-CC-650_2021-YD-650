d157f87914d46bc60441bb8c830eab6ca7dc04ad
Carlos Yanez <carlitosyanez9@gmail.com>
2024-04-20 15:07:06 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021-CC-650_2021-YD-650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021_CC_650_2021_YD_650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L02P02_2021-CC-650_2021-YD-650/Models/libreriaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace L02P02_2021_CC_650_2021_YD_650.Models
+{
+    public class libreriaDbContext : DbContext
+    {
+        public libreriaDbContext(DbContextOptions options): base (options) 
+        {
+
+        }
+    }
+}
